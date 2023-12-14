2aa807f8e3dfcdcaf2b8f77898452928510b5385
Desh Deepak Dhruv <deshdeepak.btech.iet@gmail.com>
2023-12-15 00:06:04 +0530

    add pagination

--- elevator_system/settings.py
+++ elevator_system/settings.py
@@ -101,6 +101,10 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
+}
 
 
 # Internationalization
