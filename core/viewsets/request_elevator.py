1658234d033b097fdae2f5e8a27b167220d49eaa
Desh Deepak Dhruv <deshdeepak.btech.iet@gmail.com>
2023-12-15 15:47:32 +0530

    Requested floor and destination floor cannot be same

--- core/viewsets/request_elevator.py
+++ core/viewsets/request_elevator.py
@@ -1,4 +1,8 @@
-from rest_framework import mixins, viewsets
+from typing import Any
+
+from rest_framework import mixins, status, viewsets
+from rest_framework.request import Request
+from rest_framework.response import Response
 
 from core.models import ElevatorRequest
 from core.serializers import ElevatorRequestSerializer
@@ -11,3 +15,18 @@ class ElevatorRequestViewSet(
 ):
     queryset = ElevatorRequest.objects.all()
     serializer_class = ElevatorRequestSerializer
+
+    def create(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        serializer = ElevatorRequestSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        if (
+            serializer.validated_data["requested_floor"]
+            == serializer.validated_data["destination_floor"]
+        ):
+            return Response(
+                status=status.HTTP_400_BAD_REQUEST,
+                data={
+                    "details": "Requested floor and destination floor cannot be same"
+                },
+            )
+        return super().create(request, *args, **kwargs)
