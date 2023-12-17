0f18d131958f05e4532c5fc432b4c05f29c7f291
Desh Deepak Dhruv <97728256+deshdeepak2019@users.noreply.github.com>
2023-12-17 12:08:19 +0530

    Update request_elevator.py

--- core/viewsets/request_elevator.py
+++ core/viewsets/request_elevator.py
@@ -4,7 +4,7 @@ from rest_framework import mixins, status, viewsets
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from core.models import ElevatorRequest
+from core.models import ElevatorRequest, ElevatorSystem
 from core.serializers import ElevatorRequestSerializer
 
 
@@ -21,6 +21,8 @@ class ElevatorRequestViewSet(
     def create(self, request: Request, *args: Any, **kwargs: Any) -> Response:
         serializer = ElevatorRequestSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+        system=ElevatorSystem.objects.get(elevator=serializer.validated_data["elevator"])
+        
         if (
             serializer.validated_data["requested_floor"]
             == serializer.validated_data["destination_floor"]
@@ -31,4 +33,27 @@ class ElevatorRequestViewSet(
                     "details": "Requested floor and destination floor cannot be same"
                 },
             )
+        
+        if (
+            serializer.validated_data["requested_floor"]
+            > system.max_floor
+        ):
+            return Response(
+                status=status.HTTP_400_BAD_REQUEST,
+                data={
+                    "details": "Requested floor cannot be greater than max floor in elevator system"
+                },
+            )
+        
+        if (
+            serializer.validated_data["destination_floor"]
+            > system.max_floor
+        ):
+            return Response(
+                status=status.HTTP_400_BAD_REQUEST,
+                data={
+                    "details": "Destination floor cannot be greater than max floor in elevator system"
+                },
+            )
+
         return super().create(request, *args, **kwargs)
